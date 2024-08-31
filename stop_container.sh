67c636c862a91f9430b8d62e63c280a831793dde
Shirisha Reddy <114852869+shirisha-1910@users.noreply.github.com>
2024-08-31 17:55:59 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Stopping and removing existing container..."
-docker stop sirishassss/simple-python-flask-app || true
-docker rm sirishassss/simple-python-flask-app|| true
+# Container name
+CONTAINER_NAME="sirishassss/simple-python-flask-app"
 
+# Stop and remove existing container if it exists
+if docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    echo "Stopping and removing existing container..."
+    docker stop "$CONTAINER_NAME" || true
+    docker rm "$CONTAINER_NAME" || true
+else
+    echo "No existing container found with the name $CONTAINER_NAME."
+fi
+
+# Run the new container
 echo "Running new container..."
-docker run -d -p 5001:5001 sirishassss/simple-python-flask-app
+docker run -d -p 5001:5001 --name "$CONTAINER_NAME" "$CONTAINER_NAME"
