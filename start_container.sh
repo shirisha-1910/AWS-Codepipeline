3fabb74b5373b950bb095290683cd677f380606c
krupakar0307 <krupakarreddy.yasa@gmail.com>
2024-08-31 18:55:33 +0530

    fix script

--- start_container.sh
+++ start_container.sh
@@ -4,5 +4,6 @@ set -e
 PORT=5001
 
 # Pull the Docker image from Docker Hub
-sudo docker pull sirishassss/simple-python-flask-app
-sudo docker run -d  --name pytfjfyu -p 5001:5001 sirishassss/simple-python-flask-app
+# sudo docker pull sirishassss/simple-python-flask-app
+# sudo docker run -d  --name pytfjfyu -p 5001:5001 sirishassss/simple-python-flask-app
+echo HI Now its running fine
