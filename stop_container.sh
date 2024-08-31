d1c1eec87e5a8c25fe016f688cb839b6ab8f89aa
krupakar0307 <krupakarreddy.yasa@gmail.com>
2024-08-31 18:57:43 +0530

    fix script

--- stop_container.sh
+++ stop_container.sh
@@ -6,10 +6,12 @@ IMAGE_NAME="sirishassss/simple-python-flask-app"
 # Get the container ID or name running the image
 CONTAINER_ID=$(docker ps -q -f "ancestor=$IMAGE_NAME")
 
-# Check if a container ID was found
-if [ -z "$CONTAINER_ID" ]; then
-  echo "No container found for image $IMAGE_NAME."
-else
-  echo "Stopping container with ID $CONTAINER_ID..."
-  sudo docker stop "$CONTAINER_ID"
-fi
+# # Check if a container ID was found
+# if [ -z "$CONTAINER_ID" ]; then
+#   echo "No container found for image $IMAGE_NAME."
+# else
+#   echo "Stopping container with ID $CONTAINER_ID..."
+#   sudo docker stop "$CONTAINER_ID"
+# fi
+echo Hi stopping
+
