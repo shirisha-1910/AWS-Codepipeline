4bac260e836d611f5484845c6e050a3183b44450
Shirisha Reddy <114852869+shirisha-1910@users.noreply.github.com>
2024-08-31 10:59:36 +0530

    Update start_container.sh

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -e
 
-PORT=5001
-CONTAINER_NAME="python"
-
 # Pull the Docker image from Docker Hub
-echo "Pulling Docker image..."
-docker pull sirishassss/simple-python-flask-app:latest
+echo
 
-# Run the Docker image as a new container
-echo "Running new Docker container..."
-docker run -d --name python -p 5001:5001 sirishassss/simple-python-flask-app:latest
-
-echo "Container started."
+# Run the Docker image as a container
+echo
