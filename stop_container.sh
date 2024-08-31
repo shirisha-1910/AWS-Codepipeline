154036dde41e9e98c090912b6d8e0f540d1ec6c4
Shirisha Reddy <114852869+shirisha-1910@users.noreply.github.com>
2024-08-31 17:24:12 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -2,4 +2,6 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+echo "Stopping and removing existing container..."
+docker stop sirishassss/simple-python-flask-app || true
+docker rm sirishassss/simple-python-flask-app|| true
