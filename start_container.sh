da447563b4fd98c5e7dd3845ef27c6f24c4dcf4b
krupakar0307 <krupakarreddy.yasa@gmail.com>
2024-08-31 18:45:33 +0530

    name

--- start_container.sh
+++ start_container.sh
@@ -3,6 +3,6 @@ set -e
 
 PORT=5001
 
-# Pull the Docker image from Docker Hub
+#m Pull the Docker image from Docker Hub
 docker pull sirishassss/simple-python-flask-app
-docker run -d -p 5001:5001 sirishassss/simple-python-flask-app
+docker run -d --name python -p 5001:5001 sirishassss/simple-python-flask-app
