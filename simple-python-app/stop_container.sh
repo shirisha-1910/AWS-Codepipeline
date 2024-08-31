d34e9a89402a19beedf6e3df85c5f57d6962501a
Shirisha Reddy <114852869+shirisha-1910@users.noreply.github.com>
2024-08-31 10:59:01 +0530

    Update stop_container.sh

--- simple-python-app/stop_container.sh
+++ simple-python-app/stop_container.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 set -e
 
-PORT=5001
-CONTAINER_NAME="python"
-
-# Check if the container is running and stop it
-if [ "$(docker ps -q -f name=python)" ]; then
-    echo "Stopping existing container: python"
-    docker stop python
-fi
-
-# Check if the container exists and remove it
-if [ "$(docker ps -a -q -f name=python)" ]; then
-    echo "Removing existing container: python"
-    docker rm python
-fi
-
-echo "Container stopped and removed (if it existed)."
+# Stop the running container (if any)
+echo "Hi"
