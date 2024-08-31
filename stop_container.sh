366e3f481d56cec3261e8e804e8f176f706daa1d
Shirisha Reddy <114852869+shirisha-1910@users.noreply.github.com>
2024-08-31 17:35:39 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -7,5 +7,4 @@ docker stop sirishassss/simple-python-flask-app || true
 docker rm sirishassss/simple-python-flask-app|| true
 
 echo "Running new container..."
-docker run -d -p 5001:5001 --name sirishassss/simple-python-flask-app 
-
+docker run -d -p 5001:5001 --name sirishassss_simple_python_flask_app sirishassss/simple-python-flask-app
