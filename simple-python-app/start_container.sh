3c0aaf35283c2977d653a5017df0691148bb018b
Shirisha Reddy <114852869+shirisha-1910@users.noreply.github.com>
2024-08-31 08:32:16 +0530

    Update start_container.sh

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -4,7 +4,8 @@ set -e
 PORT=5001
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sirishassss/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+dcoker run -d p 5001:5001 sirishassss/simple-python-flask-app:latest
+
