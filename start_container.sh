64083b219249bdbb3ad34b88a83abaece2ecb8fa
krupakar0307 <krupakarreddy.yasa@gmail.com>
2024-08-31 19:49:06 +0530

    test stage

--- start_container.sh
+++ start_container.sh
@@ -5,5 +5,6 @@ PORT=5001
 
 # Pull the Docker image from Docker Hub
  sudo docker pull sirishassss/simple-python-flask-app
- sudo docker run -d  --name pytfjfyu -p 5000:5000 sirishassss/simple-python-flask-app
+#  sudo docker run -d  --name pytfjfyu -p 5000:5000 sirishassss/simple-python-flask-app
+sudo docker run -d  --name pytfjfyu -p 5000:80 nginx
 echo HI Now its running fine
