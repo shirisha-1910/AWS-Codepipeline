94a5e554fb6ee0ed6fba3f0ee8160f3b568d4427
Shirisha Reddy <114852869+shirisha-1910@users.noreply.github.com>
2024-08-31 17:59:40 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,6 +5,3 @@ PORT=5001
 
 # Pull the Docker image from Docker Hub
 docker pull sirishassss/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 5001:5001 sirishassss/simple-python-flask-app
