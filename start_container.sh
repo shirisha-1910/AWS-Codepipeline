f36d4f315505c127e7ffb048a0a94e2f016b5e5d
krupakar0307 <krupakarreddy.yasa@gmail.com>
2024-09-01 16:53:32 +0530

    rename

--- start_container.sh
+++ start_container.sh
@@ -6,5 +6,5 @@ PORT=5001
 # Pull the Docker image from Docker Hub
  sudo docker pull sirishassss/simple-python-flask-app
 #  sudo docker run -d  --name pytf -p 5000:5000 sirishassss/simple-python-flask-app
-sudo docker run -d  --name pytf -p 5000:80 nginx
+sudo docker run -d  --name jhbkjnmv -p 5000:80 nginx
 echo HI Now its running fine
