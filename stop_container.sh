af92d25af319a629e623bf3c8c04b0d20e49d89f
krupakar0307 <krupakarreddy.yasa@gmail.com>
2024-09-01 17:21:52 +0530

    syntax

--- stop_container.sh
+++ stop_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Define your image name
+# Define your image name and port
 IMAGE_NAME="sirishassss/simple-python-flask-app:latest"
 PORT=5000
 
@@ -8,19 +8,19 @@ PORT=5000
 if sudo lsof -i :$PORT > /dev/null; then
   echo "Port $PORT is in use. Stopping existing container..."
 
-# Get the container ID or name running the image
-CONTAINER_ID=$(docker ps -a -q  --filter ancestor="$IMAGE_NAME")
+  # Get the container ID running the image
+  CONTAINER_ID=$(docker ps -q --filter "ancestor=$IMAGE_NAME")
 
-# Check if a container ID was found
-if [ -z "$CONTAINER_ID" ]; then
-  echo "No container found for image $IMAGE_NAME."
-else
-  echo "Stopping container with ID $CONTAINER_ID..."
-  sudo docker stop "$CONTAINER_ID"
-  sudo docker rm "$CONTAINER_ID"
-fi
-  echo Hi stopping
-else
-  echo port not in use
-fi
+  # Check if a container ID was found
+  if [ -z "$CONTAINER_ID" ]; then
+    echo "No running container found for image $IMAGE_NAME."
+  else
+    echo "Stopping container with ID $CONTAINER_ID..."
+    sudo docker stop "$CONTAINER_ID"
+    sudo docker rm "$CONTAINER_ID"
+  fi
 
+  echo "hi stopping "
+else
+  echo "Port $PORT is not in use."
+fi
